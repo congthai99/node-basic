3c48b15a6fab91661c101ad318347bd443370264
congthai99 <thaicong328@gmail.com>
2021-12-09 16:24:53 +0700

    video 8 router and MVC model

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 // const express = require('express');
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+import initWebRoute from './route/web'
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080;
 console.log('>>> check port', port)
 
+
+// setup view engine
 configViewEngine(app)
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+//init web route
+initWebRoute(app)
 
-app.get('/about', (req, res) => {
-    res.send('Hello World with Cong Thai Meo Meo')
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
