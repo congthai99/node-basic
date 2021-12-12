a3afe4a2c18c5ada49480e7fc1d451952b5fad75
congthai99 <thaicong328@gmail.com>
2021-12-12 16:13:47 +0700

    video11: router params/detail users

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import homeController from '../controller/homeController'
 
+
+// router parameters users
 let router = express.Router();
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
-
+    router.get('/detail/user/:id', homeController.getDetailPage)
     router.get('/about', (req, res) => {
         res.send('Hello World with Cong Thai Meo Meo')
     })
