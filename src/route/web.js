0f37d5125ee597e7bd8c2b1d7a3878fd34982576
congthai99 <thaicong328@gmail.com>
2021-12-18 23:30:18 +0700

    video 16 middleware

--- src/route/web.js
+++ src/route/web.js
@@ -28,6 +28,8 @@ const imageFilter = function (req, file, cb) {
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
 
+let uploadMultipleFiles = multer({ storage: storage, fileFilter: imageFilter }).array('multiple_images', 3);
+
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/detail/user/:id', homeController.getDetailPage)
@@ -38,7 +40,23 @@ const initWebRoute = (app) => {
     router.post('/update-user', homeController.postUpdateUser);
 
     router.get('/upload', homeController.getUploadFilePage);
-    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile);
+
+    router.post('/upload-multiple-images', (req, res, next) => {
+        uploadMultipleFiles(req, res, (err) => {
+            if (err instanceof multer.MulterError && err.code === "LIMIT_UNEXPECTED_FILE") {
+                // handle multer file limit error here
+                res.send('LIMIT_UNEXPECTED_FILE')
+            } else if (err) {
+                res.send(err)
+            }
+
+            else {
+                // make sure to call next() if all was well
+                next();
+            }
+        })
+    }, homeController.handleUploadMultipleFiles)
 
     router.get('/about', (req, res) => {
         res.send('Hello World with Cong Thai Meo Meo')
--- src/server.js
+++ src/server.js
@@ -6,9 +6,20 @@ import initAPIRoute from './route/api';
 // import connection from './configs/connectDB';
 require('dotenv').config()
 
+var morgan = require('morgan');
+
 const app = express()
 const port = process.env.PORT || 8080;
 console.log('>>> check port', port)
+
+app.use((req, res, next) => {
+    // check => return res.send()
+    console.log('>>> run into my middleware')
+    console.log(req.method)
+    next()
+})
+
+app.use(morgan('combined'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +33,11 @@ initWebRoute(app)
 
 //init api route
 initAPIRoute(app)
+
+// handle 404 not found error 
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
